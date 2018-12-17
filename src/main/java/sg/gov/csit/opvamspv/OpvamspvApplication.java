4998a07561a20d4bb074c395bd5f8a7114fe64a8
levi85 <joshua.zhangzy@gmail.com>
2018-12-17 20:27:04 +0800

    generated base from spring

---
+++ src/main/java/sg/gov/csit/opvamspv/OpvamspvApplication.java
@@ -0,0 +1,14 @@
+package sg.gov.csit.opvamspv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpvamspvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpvamspvApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/sg/gov/csit/opvamspv/OpvamspvApplicationTests.java
@@ -0,0 +1,17 @@
+package sg.gov.csit.opvamspv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpvamspvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
