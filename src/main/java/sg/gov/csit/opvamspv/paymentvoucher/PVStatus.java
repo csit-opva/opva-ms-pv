e5d2e5a4a3760bf1020dd8f3e59013135a6f2911
Loh Jing Yen <lohjingyen@gmail.com>
2018-12-22 17:24:16 +0800

    [WIP] Define different possible states for PV

---
+++ src/main/java/sg/gov/csit/opvamspv/paymentvoucher/PVStatus.java
@@ -0,0 +1,10 @@
+package sg.gov.csit.opvamspv.paymentvoucher;
+
+public enum PVStatus {
+    PENDING_CHECK,
+    PENDING_SUPPORTING_1,
+    PENDING_SUPPORTING_2,
+    PENDING_APPROVAL,
+    PENDING_PAYMENT,
+    PAID
+}
